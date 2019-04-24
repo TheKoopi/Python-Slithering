9a9f4989fb4ee3707f1dbd3f300dcf7c7a40ada8
Toyotomius <Toyotomius@gmail.com>
2019-04-24 02:41:02 -0300

    Benchmarking with threading

--- LotteryPy/LotteryPy.py
+++ LotteryPy/LotteryPy.py
@@ -1,37 +1,17 @@
-#Lottery Py
+import Lotto649 as L
+import LottoExample as E
+import time
+from multiprocessing import Pool
 
-import json
-import pandas
-import numpy
-import collections
+if __name__ == "__main__":
 
+    pool = Pool()
+    start = (time.time()*1000)
+    for _i in range(0, 30):
+        result1 = pool.apply_async(L.lotto649)
+        result2 = pool.apply_async(E.lottoexample)
 
-lotterylist = json.load(open(r"H:\Coding\Lotto Numbers\649\649.json"))
-test = []
-cutest = []
-a = 0
-
-for i in range(0,len(lotterylist["Lotto649"])):
-    test.append(lotterylist["Lotto649"][i]["Numbers"])
-
-# flatten list into a single list instead of list of lists.
-flat_list = [item for sublist in test for item in sublist]
-
-# use collections to count frequency of elements in flattened list.
-ctr = collections.Counter(flat_list)
-print(ctr)
-
-# use numpy to pull frequency from flattened list
-vals, counts = numpy.unique(flat_list, return_counts=True)
-#print(vals, counts)
-
-# zip the results into a dictionary of value, frequency
-results = dict(zip(vals, counts))
-
-# sorts by descending order by count
-sorted_by_value = sorted(results.items(), key=lambda kv: kv[1], reverse = True)
-#print(sorted_by_value)
-
-# Uses pandas to pull a frequency table.
-furthertest = pandas.Series(flat_list).value_counts().sort_index()
-print(furthertest)
+    pool.close()
+    pool.join()
+    end = (time.time() * 1000)
+    print(f"Total time to execute: {end - start} ms")
---
+++ LotteryPy/Lotto649.py
@@ -0,0 +1,41 @@
+import json
+import numpy
+import time
+
+
+def lotto649():
+
+    start = (time.time()*1000)
+
+    lotterylist = json.load(open("Lotto649.json"))
+    test = []
+
+    for i in range(0, len(lotterylist["Lotto649"])):
+        test.append(lotterylist["Lotto649"][i]["Numbers"])
+
+    # flatten list into a single list instead of list of lists.
+    flat_list = [item for sublist in test for item in sublist]
+
+    # # use collections to count frequency of elements in flattened list.
+    # ctr = collections.Counter(flat_list)
+    # print(ctr)
+
+    # use numpy to pull frequency from flattened list
+    vals, counts = numpy.unique(flat_list, return_counts=True)
+    # print(vals, counts)
+
+    # zip the results into a dictionary of value, frequency
+    results = dict(zip(vals, counts))
+
+    # sorts by descending order by count
+    sorted_by_value = sorted(results.items(), key=lambda kv: kv[1], reverse=True)
+    # print(sorted_by_value)
+
+    # Uses pandas to pull a frequency table.
+    # furthertest = pandas.Series(flat_list).value_counts().sort_index()
+    # print(furthertest)
+
+    with open("649Singles.txt", "w") as f:
+        f.write('\n'.join('Number: %s, Frequency: %s' % x for x in sorted_by_value))
+    end = (time.time()*1000)
+    print(f"Lotto649 Time to Execute: {end - start} ms\n")
